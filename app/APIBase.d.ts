8b1fe1d7a29ec1321901d2ff9d82723f4909da00
Emma <emma@tdm.io>
2019-11-17 23:18:52 -0400

    Setting up package.json scripts to compile on install, removing excess build code

--- app/APIBase.d.ts
+++
@@ -1,8 +0,0 @@
-import { ChastiKeyEndpoint, IChastiKeyOptions } from './ChastiKey';
-export declare class APIBase {
-    config: IChastiKeyOptions;
-    constructor(conf?: IChastiKeyOptions);
-    get baseURLBuilt(): string;
-    protected paramsBuilder(params?: any): string;
-    protected request<T, I>(endpoint: ChastiKeyEndpoint, params?: I): Promise<T>;
-}
--- app/errors.d.ts
+++
@@ -1,4 +0,0 @@
-export declare class FetchError extends Error {
-    code: number;
-    constructor(code: number, message?: string);
-}
